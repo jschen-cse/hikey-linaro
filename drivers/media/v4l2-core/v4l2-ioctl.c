18423a52fdc4027a1e9df1efbce85190295996ab
Hans Verkuil <hans.verkuil@cisco.com>
2018-10-04 16:48:12 -0400

    UPSTREAM: media: v4l2-ioctl: remove unused vidioc_g/s_crop
    Now that all drivers have dropped vidioc_g/s_crop we can remove
    support for them in the V4L2 core.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Tested-by: Sylwester Nawrocki <s.nawrocki@samsung.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>
    (cherry picked from commit ed3e2749ff4a94e35de653f9bbce407eb25ae363)
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>
    
    Change-Id: I49f1fd1bc9f7a631f1a341b61a135d311eaaa50a

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -2211,8 +2211,6 @@ static int v4l_g_crop(const struct v4l2_ioctl_ops *ops,
 	};
 	int ret;
 
-	if (ops->vidioc_g_crop)
-		return ops->vidioc_g_crop(file, fh, p);
 	/* simulate capture crop using selection api */
 
 	/* crop means compose for output devices */
@@ -2243,8 +2241,6 @@ static int v4l_s_crop(const struct v4l2_ioctl_ops *ops,
 		.r = p->c,
 	};
 
-	if (ops->vidioc_s_crop)
-		return ops->vidioc_s_crop(file, fh, p);
 	/* simulate capture crop using selection api */
 
 	/* crop means compose for output devices */
