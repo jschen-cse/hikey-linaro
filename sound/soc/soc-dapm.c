cec830b229c7e5c804f7eb1760ab9f9dd477e880
Jerome Brunet <jbrunet@baylibre.com>
2019-07-25 18:59:49 +0200

    UPSTREAM: ASoC: codec2codec: fill some of the runtime stream parameters
    Set the information provided struct snd_soc_pcm_stream in the
    struct snd_pcm_runtime of the codec to codec link.
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>
    Link: https://lore.kernel.org/r/20190725165949.29699-7-jbrunet@baylibre.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 9de98628c895d15427138073986eab1e3ce39cb4
     git://git.kernel.org/pub/scm/linux/kernel/git/broonie/sound.git for-5.4)
    
    Change-Id: I515e69d873e6d8acb696a89af5f6dffcf1e8a5ce

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -3755,6 +3755,11 @@ snd_soc_dai_link_event_pre_pmu(struct snd_soc_dapm_widget *w,
 			goto out;
 	}
 
+	runtime->format = params_format(params);
+	runtime->subformat = params_subformat(params);
+	runtime->channels = params_channels(params);
+	runtime->rate = params_rate(params);
+
 out:
 	if (ret < 0)
 		kfree(runtime);
